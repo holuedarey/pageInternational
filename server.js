5c8a5d0f84904a3fecd297e285f16a0ef062ea56
Ogunwole Samuel <samuel.ogunwole@venturegardengroup.com>
2022-01-24 10:22:11 +0100

    ready for deployment

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+// Constants
+const PORT = process.env.PORT || 4300;
+
+// App
+const app = express();
+
+// app.use(express.static('dist/dist'));
+app.use(express.static('dist'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('./dist/index.html'));
+});
+
+app.listen(PORT);
+console.log(`Running on http://localhost:${PORT}`);
